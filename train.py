a420023a2141d7d897d5f11fff0d5c732ad90c80
NOKUBI Takatsugu <knok@daionet.gr.jp>
2017-09-13 14:30:30 +0900

    optimizer, updater

--- train.py
+++ train.py
@@ -9,22 +9,89 @@ import chainer
 from data import Pix2pixDataset
 from net import UNetGenerator, P2PDiscriminator
 
+class Pix2pixUpdater(chainer.training.StandardUpdater):
+    def __init__(self, *args, **kwargs):
+        self.gen, self.dis = kwargs.pop('models')
+        super(Pix2pixUpdater, self).__init__(*args, **kwargs)
+
+    def loss_gen(self, gen, x_out, t_out, y_out, lam1=100, lam2=1):
+        batchsize, _, w, h = y_out.data.shape
+        loss_rec = lam1*(F.mean_absolute_error(x_out, t_out))
+        loss_adv = lam2*F.sum(F.softplus(-y_out)) / batchsize / w / h
+        loss = loss_rec + loss_adv
+        chainer.report({'loss': loss}, gen)
+        return loss
+
+    def loss_dis(self, dis, y_in, y_out):
+        batchsize, _, w, h = y_in.data.shape
+
+        L1 = F.sum(F.softplus(-y_in)) / batchsize / w / h
+        L2 = F.sum(F.softplus(yout)) / batchsize / w / h
+        loss = L1 + L2
+        chainer.report({'loss': loss}, dis)
+        return loss
+
+    def update_core(self):
+        gen_opt = self.get_optimizer('gen')
+        dis_opt = self.get_optimizer('dis')
+        gen, dis = self.gen, self.dis
+        xp = gen.xp
+
+        batch = self.get_iterator('main').next()
+        batchsize = len(batch)
+        in_ch = batch[0][0].shape[0]
+        out_ch = batch[0][1].shape[0]
+        w_in = 256
+        w_out = 256
+
+        x_in = xp.zeros((batchsize, in_ch, w_in, w_in)).astype('f')
+        y_out = xp.zeros((batchsize, out_ch, w_in, w_in)).astype('f')
+
+        for i in range(batchsize):
+            x_in[i, :] = xp.asarray(batch[i][0])
+            t_out[i, :] = xp.asarray(batch[i][1])
+        
+        x_in = chainer.Variable(x_in)
+        x_out = gen(x_in)
+
+        y_fake = dis(x_in, x_out)
+        y_real = dis(x_in, t_out)
+
+        gen_opt.update(self.loss_gen, gen, x_out, t_out, y_fake)
+        x_in.unchain_backward()
+        x_out.unchain_backward()
+        dis_opt.update(self.loss_dis, dis, y_real, y_fake)
+
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--gpu", "-g", type=int, default=-1)
     parser.add_argument("--input-dir", type=str, required=True)
     parser.add_argument("--output-dir", type=str, required=True)
+    parser.add_argument("--batchsize", type=int, default=1)
     parser.add_argument("--ngf", type=int, default=64)
     parser.add_argument("--ndf", type=int, default=64)
+    parser.add_argument("--lr", type=float, default=0.0002)
+    parser.add_argument("--beta1", type=float, default=0.5)
     args = parser.parse_args()
     return args
 
 def main():
     args = get_args()
 
-    unet = UNetGenerator(args.ngf)
-    disc = P2PDiscriminator(args.ndf)
+    gen = UNetGenerator(args.ngf)
+    dis = P2PDiscriminator(args.ndf)
     train_data = Pix2pixDataset(args.input_dir)
+    train_iter = chainer.iterators.SerialIterator(train_data, args.batchsize)
+    opt_gen = chainer.optimizers.Adam(alpha=args.lr, beta1=args.beta1)
+    opt_gen.setup(gen)
+    opt_dis = chainer.optimizers.Adam(alpha=args.lr, beta1=args.beta1)
+    opt_dis.setup(dis)
+
+    updater = Pix2pixUpdater(models=(gen, dis),
+                             iterator={'main': train_data},
+                             optimizer={'gen': opt_gen,
+                                        'dis': opt_dis},
+                             device=args.gpu)
 
 if __name__ == '__main__':
     main()
