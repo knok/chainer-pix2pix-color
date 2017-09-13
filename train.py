9f6aaaed287946eef919ae10da49502617684308
NOKUBI Takatsugu <knok@daionet.gr.jp>
2017-09-13 13:49:25 +0900

    build models and dataset

--- train.py
+++ train.py
@@ -14,11 +14,17 @@ def get_args():
     parser.add_argument("--gpu", "-g", type=int, default=-1)
     parser.add_argument("--input-dir", type=str, required=True)
     parser.add_argument("--output-dir", type=str, required=True)
+    parser.add_argument("--ngf", type=int, default=64)
+    parser.add_argument("--ndf", type=int, default=64)
     args = parser.parse_args()
     return args
 
 def main():
     args = get_args()
+
+    unet = UNetGenerator(args.ngf)
+    disc = P2PDiscriminator(args.ndf)
+    train_data = Pix2pixDataset(args.input_dir)
 
 if __name__ == '__main__':
     main()
